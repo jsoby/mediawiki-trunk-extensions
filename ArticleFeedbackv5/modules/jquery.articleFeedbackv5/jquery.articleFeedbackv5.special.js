jsoby/mediawiki-trunk-extensions

commit 8fead3f9ad6091db77cb4e925897ba729c3af141

AFT5 - fix issues raised on r110326 - whitespace instead of tabs, inefficient regex in JS, and wrong function to build permalink.
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions@110402 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

ArticleFeedbackv5/modules/jquery.articleFeedbackv5/jquery.articleFeedbackv5.special.js
@@ -85,7 +85,7 @@
 
 		// Process anything we found in the URL hash
 		// Permalinks.
-		var id = window.location.href.match(/(.+)\/(\d+)$/)
+		var id = window.location.href.match(/\/(\d+)$/)
 		if( id ) {
 			$.articleFeedbackv5special.listControls.filter      = 'id';
 			$.articleFeedbackv5special.listControls.filterValue = id[2];

ArticleFeedbackv5/api/ApiViewFeedbackArticleFeedbackv5.php
@@ -468,36 +468,34 @@ class ApiViewFeedbackArticleFeedbackv5 extends ApiQueryBase {
 	}
 
 	private function renderPermalinkTimestamp( $record ) {
-	    global $wgLang;
-	    $id    = $record->af_id;
+		global $wgLang;
+		$id    = $record->af_id;
 		$title = $record->page_title;
 
-	    // Taken from the Moodbar extension.
-	    $now       = wfTimestamp( TS_UNIX );
-	    $timestamp = wfTimestamp( TS_UNIX, $record->af_created );
-	    $date	   = '';
+		// Taken from the Moodbar extension.
+		$now       = wfTimestamp( TS_UNIX );
+		$timestamp = wfTimestamp( TS_UNIX, $record->af_created );
+		$date	   = '';
 
-	    // Relative dates for 48 hours, normal timestamps later.
-	    if ( $timestamp > ( $now - ( 86400 * 2 ) ) ) {
-		    $time = $wgLang->formatTimePeriod(
-			    ( $now - $timestamp ), 'avoidseconds'
-		    );
-		    $date = wfMessage( 'articleFeedbackv5-comment-ago', $time )->escaped();
-	    } elseif( $timestamp ) {
-		    $date = $wgLang->timeanddate($record->af_created  );
-	    }
+		// Relative dates for 48 hours, normal timestamps later.
+		if ( $timestamp > ( $now - ( 86400 * 2 ) ) ) {
+			$time = $wgLang->formatTimePeriod(
+				( $now - $timestamp ), 'avoidseconds'
+			);
+			$date = wfMessage( 'articleFeedbackv5-comment-ago', $time )->escaped();
+			} elseif( $timestamp ) {
+				$date = $wgLang->timeanddate($record->af_created  );
+		}
 
-	    // format the element
-	    return Html::openElement( 'span', array(
-		    'class' => 'articleFeedbackv5-comment-details-date'
-	    ) )
+		// format the element
+		return Html::openElement( 'span', array(
+			'class' => 'articleFeedbackv5-comment-details-date'
+		) )
 		. Linker::link(
-			Title::newFromText(
-				"Special:ArticleFeedbackv5/$title/$id"
-			),
+			SpecialPage::getTitleFor( 'ArticleFeedbackv5', "$title/$id" ),
 			$date
 		)
-	    . Html::closeElement( 'span' );
+		. Html::closeElement( 'span' );
 	}
 
 	private function renderBucket1( $record ) {

commit 4ae72e6e96a45c120af3ce8300e01dc4b9fd588b

AFT5 - fix bug introduced in r110402 - wrong offset in regex return
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions@110403 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

ArticleFeedbackv5/modules/jquery.articleFeedbackv5/jquery.articleFeedbackv5.special.js
@@ -88,7 +88,7 @@
 		var id = window.location.href.match(/\/(\d+)$/)
 		if( id ) {
 			$.articleFeedbackv5special.listControls.filter      = 'id';
-			$.articleFeedbackv5special.listControls.filterValue = id[2];
+			$.articleFeedbackv5special.listControls.filterValue = id[1];
 		}
 
 		// Bold the default sort.