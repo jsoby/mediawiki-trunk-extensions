9d81cb8e2265d208f4fb0a64b3f51d96843d9e93
wikinaut <wikinaut@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2012-02-20 23:21:21 +0000

    version number update 1.001
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions@111981 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- OpenID/OpenID.php
+++ OpenID/OpenID.php
@@ -27,7 +27,7 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 	exit( 1 );
 }
 
-define( 'MEDIAWIKI_OPENID_VERSION', '1.000 20120212' );
+define( 'MEDIAWIKI_OPENID_VERSION', '1.001 20120220' );
 
 $path = dirname( __FILE__ );
 set_include_path( implode( PATH_SEPARATOR, array( $path ) ) . PATH_SEPARATOR . get_include_path() );
