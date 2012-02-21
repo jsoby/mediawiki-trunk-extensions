4ba203448d363dbb4fc43903f69cc2f39df320f9
amire80 <amire80@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2012-02-21 04:55:52 +0000

    Adding more tests for initialization, transliteration and cookies.
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions@111990 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Narayam/tests/qunit/ext.narayam.tests.js
+++ Narayam/tests/qunit/ext.narayam.tests.js
@@ -11,21 +11,31 @@
 
 module( "ext.narayam", QUnit.newMwEnvironment() );
 
-// A language that definitely has input methods
-mw.config.set( { wgUserLanguage: "ml" } );
-
 function setup() {
 	$.narayam.setup();
 	$.narayam.enable();
 }
+
 function teardown() {
-	// we need to disable narayam, otherwise many typing simulation based test eg: jquery.byteLimitTest will fail.
+	// We need to disable narayam, otherwise many typing simulation based
+	// tests, like jquery.byteLimitTest, will fail.
 	$.narayam.disable();
 }
-test( "-- Initial check", function() {
-	expect( 1 );
 
+test( "-- Initial check", function() {
+	expect( 2 );
+
+	// Clean up before testing initialization
+	teardown();
+	// Delete state cookie
+	$.cookie( 'narayam-enabled', null );
+
+	setup();
 	ok( $.narayam, "$.narayam is defined" );
+	equals( $.narayam.enabled(), mw.config.get( 'wgNarayamEnabledByDefault' ), 'Initial state of Narayam matches the configuration' );
+
+	// Init empty cookie of recent schemes
+	$.cookie( 'narayam-scheme', '', { path: '/', expires: 30 } );
 } );
 
 test( "-- Initialization functions", function() {
@@ -74,6 +84,56 @@ test( '-- Build the menu', function() {
 	ok(  $.narayam.buildMenu( ), 'Build the menu again' );
 	equals( $( 'li#pt-narayam' ).length, 1, 'There should be one and only one menu at any time' );
 	equals( $( 'li.narayam-help-link' ).length, 1, 'Help link exists' );
+	teardown();
+} );
+
+test( '-- German transliteration and keybuffers', function() {
+	expect( 3 );
+	setup();
+
+	// Testing keybuffer ("compose key")
+	$.narayam.setScheme( 'de' );
+	equals( $.narayam.transliterate( '~o', '~', false ), 'ö', 'German ~o -> ö' );
+	equals( $.narayam.transliterate( '~O', '~', false ), 'Ö', 'German ~O -> Ö' );
+	equals( $.narayam.transliterate( '~s', '~', false ), 'ß', 'German ~s -> ß' );
+
+	teardown();
+} );
+
+test( '-- Hebrew transliteration, extended keyboard', function() {
+	expect( 2 );
+	setup();
+
+	// Testing extended and non-extended
+	$.narayam.setScheme( 'he-standard-2011-extonly' );
+	equals( $.narayam.transliterate( '=', '', false ), '=', 'Hebrew non-extended = does not change' );
+	equals( $.narayam.transliterate( '=', '', true ), '–', 'Hebrew extended = becomes en dash' );
+
+	teardown();
+} );
+
+test( '-- Malayalam transliteration, cookie, zwnj, longer keybuffers', function() {
+	expect( 8 );
+	setup();
+
+	$.narayam.setScheme( 'kn' );
+	var recentSchemes = $.cookie( 'narayam-scheme' ),
+		currentSchemeRegex = new RegExp( '^kn' );
+	ok ( currentSchemeRegex.test( recentSchemes ), 'New scheme added to the cookie' );
+
+	$.narayam.setScheme( 'ml' );
+
+	equals( $.narayam.transliterate( 'a',  '', false ), 'അ', 'Malayalam a -> അ' );
+
+	// N.B.: There's a zwnj in the input, and no zwnj in the expected result
+	equals( $.narayam.transliterate( 'നീല‌a',  '', false ), 'നീലഅ', 'Malayalam zwnj+a -> അ' );
+	equals( $.narayam.transliterate( 'ൻൿh',  'nc', false ), 'ഞ്ച്', 'Malayalam nch -> ഞ്ച്' );
+
+	equals( $.narayam.transliterate( 'p', '', false ), 'പ്', 'Malayalam p -> പ്' );
+	equals( $.narayam.transliterate( 'പ്a', '', false ), 'പ', 'Malayalam pa -> പ' );
+	equals( $.narayam.transliterate( 'ക്h', '', false ), 'ഖ്', 'Malayalam kh -> ഖ്' );
+	equals( $.narayam.transliterate( 'ഖ്a', '', false ), 'ഖ', 'Malayalam kha -> ഖ്' );
+
 	teardown();
 } );
 
