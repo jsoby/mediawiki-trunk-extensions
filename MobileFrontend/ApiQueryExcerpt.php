jsoby/mediawiki-trunk-extensions

commit b82b3bb24f0129a3045958bac2f501ecab218b76

Fixed bug with earlier versions of libxml
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions@112303 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

MobileFrontend/ApiQueryExcerpt.php
@@ -54,7 +54,7 @@ class ApiQueryExcerpt extends ApiQueryBase {
 	 */
 	private function processText( $text, Title $title, $plainText ) {
 		$text = preg_replace( '/<h[1-6].*$/s', '', $text );
-		$mf = new MobileFormatter( $text, $title, 'XHTML' );
+		$mf = new MobileFormatter( MobileFormatter::wrapHTML( $text ), $title, 'XHTML' );
 		$mf->removeImages();
 		$mf->remove( array( 'table', 'div', 'sup.reference', 'span.coordinates', 'span.geo-multi-punct', 'span.geo-nondefault' ) );
 		if ( $plainText ) {

MobileFrontend/MobileFormatter.php
@@ -99,6 +99,15 @@ class MobileFormatter {
 	}
 
 	/**
+	 * Wraps a chunk of HTML into 
+	 * @param string $html
+	 * @return string
+	 */
+	public static function wrapHTML( $html ) {
+		return '<!doctype html><html><head></head><body><div id="content">' . $html . '</div></body></html>';
+	}
+
+	/**
 	 * Use the given message cache
 	 * @param Array $messages
 	 */

MobileFrontend/ApiParseExtender.php
@@ -72,7 +72,7 @@ class ApiParseExtender {
 				);
 				$context->setUseFormat( 'wml' ); // Force WML links just in case
 				$context->setOnlyThisSegment( isset( $params['section'] ) );
-				$mf = new MobileFormatter( '<body><div id="content">' . $data['parse']['text']['*'] . '</div></body>',
+				$mf = new MobileFormatter( MobileFormatter::wrapHTML( $data['parse']['text']['*'] ),
 					$title,
 					ExtMobileFrontend::parseContentFormat( $params['mobileformat'] ),
 					$context

commit 554512ae4540c2607624cd4d4b2363b8b4c1f64a

prop=excerpt: * Fixed breakage from r112303 * Added a limit * svn:keywords: Id
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions@112319 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

MobileFrontend/ApiQueryExcerpt.php
@@ -13,12 +13,29 @@ class ApiQueryExcerpt extends ApiQueryBase {
 			return;
 		}
 		$params = $this->extractRequestParams();
+		$continue = 0;
+		if ( isset( $params['continue'] ) ) {
+			$continue = intval( $params['continue'] );
+			if ( $continue < 0 || $continue > count( $titles ) ) {
+				$this->dieUsageMsg( '_badcontinue' );
+			}
+			$titles = array_slice( $titles, $continue, null, true );
+		}
+		$count = 0;
 		foreach ( $titles as $id => $t ) {
+			if ( ++$count > $params['limit'] ) {
+				$this->setContinueEnumParameter( 'continue', $continue + $count - 1 );
+				break;
+			}
 			$text = $this->getExcerpt( $t, $params['plaintext'] );
 			if ( isset( $params['length'] ) ) {
 				$text = $this->trimText( $text, $params['length'] );
 			}
-			$this->addPageSubItem( $id, $text );
+			$fit = $this->addPageSubItem( $id, $text );
+			if ( !$fit ) {
+				$this->setContinueEnumParameter( 'continue', $continue + $count - 1 );
+				break;
+			}
 		}
 	}
 
@@ -54,7 +71,7 @@ class ApiQueryExcerpt extends ApiQueryBase {
 	 */
 	private function processText( $text, Title $title, $plainText ) {
 		$text = preg_replace( '/<h[1-6].*$/s', '', $text );
-		$mf = new MobileFormatter( MobileFormatter::wrapHTML( $text ), $title, 'XHTML' );
+		$mf = new MobileFormatter( MobileFormatter::wrapHTML( $text, false ), $title, 'XHTML' );
 		$mf->removeImages();
 		$mf->remove( array( 'table', 'div', 'sup.reference', 'span.coordinates', 'span.geo-multi-punct', 'span.geo-nondefault' ) );
 		if ( $plainText ) {
@@ -104,7 +121,7 @@ class ApiQueryExcerpt extends ApiQueryBase {
 			),
 			'plaintext' => false,
 			'continue' => array(
-				ApiBase::PARAM_TYPE => 'string',
+				ApiBase::PARAM_TYPE => 'integer',
 			),
 		);
 	}
@@ -140,7 +157,7 @@ class ApiQueryExcerpt extends ApiQueryBase {
 	}
 
 	public function getVersion() {
-		return __CLASS__ . ': $Id: ApiQueryCoordinates.php 110649 2012-02-03 10:18:20Z maxsem $';
+		return __CLASS__ . ': $Id$';
 	}
 }
 

MobileFrontend/MobileFormatter.php
@@ -99,12 +99,16 @@ class MobileFormatter {
 	}
 
 	/**
-	 * Wraps a chunk of HTML into 
+	 * Turns a chunk of HTML into a proper document
 	 * @param string $html
+	 * @param bool $contentDiv
 	 * @return string
 	 */
-	public static function wrapHTML( $html ) {
-		return '<!doctype html><html><head></head><body><div id="content">' . $html . '</div></body></html>';
+	public static function wrapHTML( $html, $contentDiv = true ) {
+		if ( $contentDiv ) {
+			$html = '<div id="content">' . $html . '</div>';
+		}
+		return '<!doctype html><html><head></head><body>' . $html . '</body></html>';
 	}
 
 	/**